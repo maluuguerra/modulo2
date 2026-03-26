a2f9eec31d0b6f2cc9406b22198a620e197724d9
maluuguerra <maria.guerra@pditabira.com>
2026-03-26 11:13:15 -0300

    Delete aula4_questao2.py

--- aula4_questao2.py
+++
@@ -1,7 +0,0 @@
-F = float(input('Temperatura em Fahrenheit: ')) #recebendo o valor em fahrenheits
-C = (F -32) * (5/9) #conversão para Celsius
-
-C = int(C) # conversão de float para int
-
-print('%.0f graus Fahrenheit são %d graus Ceusius' % (F,C))
-
